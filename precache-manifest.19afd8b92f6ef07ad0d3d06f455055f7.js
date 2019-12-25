c37dc360a3fe4d87cb5af427868736f93e20538f
Michael <michaelfromyeg@gmail.com>
2019-12-25 11:04:33 -0700

    Temp fix

---
+++ precache-manifest.19afd8b92f6ef07ad0d3d06f455055f7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ddb2730876e57a25d76fc76bb1a10a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e04efab5ad938b72ef6",
+    "url": "/static/css/main.6970dade.chunk.css"
+  },
+  {
+    "revision": "a5e180b0978695123997",
+    "url": "/static/js/2.83de2a38.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.83de2a38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e04efab5ad938b72ef6",
+    "url": "/static/js/main.38686315.chunk.js"
+  },
+  {
+    "revision": "fd103d41066edfe2edd7",
+    "url": "/static/js/runtime-main.06363229.js"
+  },
+  {
+    "revision": "0fe5a27204cb9f63fc5af74d10c3d040",
+    "url": "/static/media/UnicaOne-Regular.0fe5a272.ttf"
+  },
+  {
+    "revision": "a7ed957962fb0d24667544b26c8bb125",
+    "url": "/static/media/Vollkorn-Bold.a7ed9579.ttf"
+  },
+  {
+    "revision": "7fb0386b732bf2e4cb91dedae8da6840",
+    "url": "/static/media/Vollkorn-BoldItalic.7fb0386b.ttf"
+  },
+  {
+    "revision": "0541820d17ec3ac6ec8b6b8803ca4cb2",
+    "url": "/static/media/Vollkorn-Italic.0541820d.ttf"
+  },
+  {
+    "revision": "e1d944914cf2c86a31244dada1ef9d0c",
+    "url": "/static/media/Vollkorn-Regular.e1d94491.ttf"
+  },
+  {
+    "revision": "1b8df32dd6f47e609bac827203636360",
+    "url": "/static/media/headshot.1b8df32d.png"
+  },
+  {
+    "revision": "415d637a70303e6fc77abe9ff1d76cbe",
+    "url": "/static/media/resume.415d637a.pdf"
+  }
+]);
